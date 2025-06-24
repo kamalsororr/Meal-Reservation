00bb6e857f35e961783c5bd1daa3bfc8fa499ff1
Kamal Soror <92731223+kamalsororr@users.noreply.github.com>
2025-06-24 18:27:30 +0300

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwn5S3IPh5-b7_pld0ftBMkahUL_mqncA",
+  authDomain: "mealreservation709.firebaseapp.com",
+  projectId: "mealreservation709",
+  storageBucket: "mealreservation709.firebasestorage.app",
+  messagingSenderId: "594633753192",
+  appId: "1:594633753192:web:56b8963787aededb4a71dd",
+  measurementId: "G-WPGXF3QF72"
+};
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+function checkLogin() {
+    const nationalId = document.getElementById("nationalIdInput").value.trim();
+    const name = document.getElementById("nameInput").value.trim();
+    const msg = document.getElementById("loginMessage");
+
+    if (!nationalId || !name) {
+        msg.innerText = "يجب إدخال الاسم والرقم القومي";
+        return;
+    }
+
+    document.getElementById("userName").value = name;
+    localStorage.setItem("nationalId", nationalId);
+    localStorage.setItem("userName", name);
+    msg.innerText = "✅ تم تسجيل الدخول بنجاح";
+    document.getElementById("login-section").style.display = "none";
+    document.getElementById("reservation-section").style.display = "block";
+}
+
+function submitReservation() {
+    const nationalId = localStorage.getItem("nationalId");
+    const name = localStorage.getItem("userName");
+    const mealChecked = document.getElementById("mealCheck").checked;
+    const msg = document.getElementById("reservationMessage");
+
+    const now = new Date();
+    const hour = now.getHours();
+
+    if (hour >= 6 && hour < 14) {
+        msg.innerText = "❌ لا يمكن تعديل الحجز من الساعة 6 صباحًا حتى 2 ظهرًا.";
+        return;
+    }
+
+    const day = now.getDate();
+
+    firebase.database().ref("reservations/" + nationalId).set({
+        name: name,
+        ["day_" + day]: mealChecked
+    }).then(() => {
+        msg.innerText = "✅ تم الحجز بنجاح";
+    }).catch(error => {
+        msg.innerText = "حدث خطأ أثناء الحجز";
+        console.error(error);
+    });
+}
